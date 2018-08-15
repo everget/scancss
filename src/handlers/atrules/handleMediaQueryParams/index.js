b71fad4ed9fd33da1f3e22e15ff43abcdcee32bd
everget <alex.or161@mail.ru>
2018-08-15 22:50:26 +0300

    Fix counting of comma-separated media queries

--- src/handlers/atrules/handleMediaQueryParams/index.js
+++ src/handlers/atrules/handleMediaQueryParams/index.js
@@ -1,61 +1,68 @@
+import postcss from 'postcss';
+
 import { cssBrowserHacks } from '../../../constants/cssBrowserHacks';
 import { reCssMediaFeature } from '../../../constants/reCssMediaFeature';
 import { reCssMediaType } from '../../../constants/reCssMediaType';
 import { rePrefixedString } from '../../../constants/rePrefixedString';
 import { countUsage } from '../../../calculators/countUsage';
-import { removeExtraSpaces } from '../../../converters/removeExtraSpaces';
+import { trimExtraSpaces } from '../../../converters/trimExtraSpaces';
 import { handleVendorPrefix } from '../../handleVendorPrefix';
 
 const reOnlyKeyword = /\bonly\b/g;
 const cssMediaBrowserHacks = cssBrowserHacks.filter((hack) => hack.type === 'media');
 
 export function handleMediaQueryParams(params, report, options) {
-	report.mediaQueries.total++;
+	postcss
+		.list
+		.comma(params)
+		.forEach((mediaQuery) => {
+			report.mediaQueries.total++;
 
-	const cleanedParams = removeExtraSpaces(params);
+			const cleanedMediaQuery = trimExtraSpaces(mediaQuery);
 
-	if (reOnlyKeyword.test(cleanedParams)) {
-		report.mediaQueries.onlyKeyword += cleanedParams.match(reOnlyKeyword).length;
-	}
+			if (reOnlyKeyword.test(cleanedMediaQuery)) {
+				report.mediaQueries.onlyKeyword += cleanedMediaQuery.match(reOnlyKeyword).length;
+			}
 
-	if (reCssMediaType.test(cleanedParams)) {
-		cleanedParams
-			.match(reCssMediaType)
-			.forEach((type) => {
-				report.mediaQueries.types.total++;
-				countUsage(type, report.mediaQueries.types.usage);
-			});
-	}
-
-	if (reCssMediaFeature.test(cleanedParams)) {
-		cleanedParams
-			.match(reCssMediaFeature)
-			.forEach((feature) => {
-				if (/^(min|max)--moz-/.test(feature)) {
-					report.mediaQueries.features.prefixed++;
-					handleVendorPrefix('-moz-', report);
-				} else if (rePrefixedString.test(feature)) {
-					report.mediaQueries.features.prefixed++;
-					handleVendorPrefix(feature, report);
-				}
-
-				report.mediaQueries.features.total++;
-				countUsage(feature, report.mediaQueries.features.usage);
-			});
-	}
-
-	countUsage(cleanedParams, report.mediaQueries.usage);
-
-	if (options.browserHacks) {
-		cssMediaBrowserHacks.forEach((hack) => {
-			if (hack.regex.test(cleanedParams)) {
-				cleanedParams
-					.match(hack.regex)
-					.forEach((match) => {
-						report.browserHacks.total++;
-						countUsage(match, report.browserHacks.usage.media);
+			if (reCssMediaType.test(cleanedMediaQuery)) {
+				cleanedMediaQuery
+					.match(reCssMediaType)
+					.forEach((type) => {
+						report.mediaQueries.types.total++;
+						countUsage(type, report.mediaQueries.types.usage);
 					});
 			}
+
+			if (reCssMediaFeature.test(cleanedMediaQuery)) {
+				cleanedMediaQuery
+					.match(reCssMediaFeature)
+					.forEach((feature) => {
+						if (/^(min|max)--moz-/.test(feature)) {
+							report.mediaQueries.features.prefixed++;
+							handleVendorPrefix('-moz-', report);
+						} else if (rePrefixedString.test(feature)) {
+							report.mediaQueries.features.prefixed++;
+							handleVendorPrefix(feature, report);
+						}
+
+						report.mediaQueries.features.total++;
+						countUsage(feature, report.mediaQueries.features.usage);
+					});
+			}
+
+			countUsage(cleanedMediaQuery, report.mediaQueries.usage);
+
+			if (options.browserHacks) {
+				cssMediaBrowserHacks.forEach((hack) => {
+					if (hack.regex.test(cleanedMediaQuery)) {
+						cleanedMediaQuery
+							.match(hack.regex)
+							.forEach((match) => {
+								report.browserHacks.total++;
+								countUsage(match, report.browserHacks.usage.media);
+							});
+					}
+				});
+			}
 		});
-	}
 }
