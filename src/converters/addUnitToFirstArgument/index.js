a7a5d01e03bd06858b4722e416c25bf919dc7075
everget <alex.or161@mail.ru>
2018-09-05 22:49:53 +0300

    Remove unnecessary converters

--- src/converters/addUnitToFirstArgument/index.js
+++
@@ -1,7 +0,0 @@
-import { reNumberString } from '../../constants/reNumberString';
-
-const reNumberStringSource = reNumberString.source.slice(1, -1);
-
-export function addUnitToFirstArgument(str, unit) {
-	return str.replace(new RegExp('(\\(' + reNumberStringSource + ')', 'g'), '$1' + unit);
-}
